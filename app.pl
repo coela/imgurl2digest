8fb120497e781a1d246bce3110c6692b8ab9de79
coela <coela.st@gmail.com>
2011-06-16 15:12:14 +0900

    created app

---
+++ app.pl
@@ -0,0 +1,26 @@
+use 5.12.2;
+use Mojolicious::Lite;
+use Data::Dumper;
+use Furl;
+use Digest::SHA1  qw(sha1 sha1_hex sha1_base64);
+use Mojo::JSON;
+
+get '/' => sub{
+	my $self = shift;
+	my $url = $self->param('url');
+	my $furl = Furl->new(
+			agent   => 'MyGreatUA/2.0',
+			timeout => 10,
+			);
+
+	my $res = $furl->get($url);
+	die $res->status_line unless $res->is_success;
+
+	my $sha1 = Digest::SHA1->new;
+	$sha1->add($res->content);
+	my $json   = Mojo::JSON->new;
+	my $string = $json->encode($sha1->hexdigest);
+	$self->render(text => $string);
+};
+
+app->start;
