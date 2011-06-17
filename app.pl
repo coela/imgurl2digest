0cccb7b68f3c37f3ed44ad9ddaa86bb0c3368b19
coela <coela.st@gmail.com>
2011-06-17 14:22:22 +0900

    return Gyazz content

--- app.pl
+++ app.pl
@@ -4,6 +4,8 @@ use Mojolicious::Lite;
 use Furl;
 use Digest::SHA1  qw(sha1 sha1_hex sha1_base64);
 use Mojo::JSON;
+use Encode;
+
 #use Mojo::Headers;
 #my $headers = Mojo::Headers->new;
 #$headers = $headers->add('Access-Control-Allow-Origin', '*');
@@ -22,9 +24,10 @@ get '/' => sub{
 	my $sha1 = Digest::SHA1->new;
 	$sha1->add($res->content);
 	my $json   = Mojo::JSON->new;
-#my $string = $json->encode($sha1->hexdigest);
+	my $string = $sha1->hexdigest;
+	my $res2 = $furl->get("http://gyazz.pixcell.dotcloud.com/Gyazz/image/$string/text/0");
 	$self->res->headers->header('Access-Control-Allow-Origin' => '*');
-	$self->render(text=>$sha1->hexdigest);
+	$self->render(text=> decode("utf8",$res2->content));
 };
 
 app->start;
